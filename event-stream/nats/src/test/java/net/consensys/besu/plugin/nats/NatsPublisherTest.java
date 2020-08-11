48527a3a2711fc4df39388c2937fa88bc4eb0ffd
Karim TAAM <karim.t2am@gmail.com>
2020-08-11 11:42:11 +0200

    add tests
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- event-stream/nats/src/test/java/net/consensys/besu/plugin/nats/NatsPublisherTest.java
+++ event-stream/nats/src/test/java/net/consensys/besu/plugin/nats/NatsPublisherTest.java
@@ -12,11 +12,9 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package net.consensys.besu.plugin.kafka;
+package net.consensys.besu.plugin.nats;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 
 import net.consensys.besu.plugins.stream.api.event.Event;
@@ -25,42 +23,34 @@ import net.consensys.besu.plugins.stream.api.event.TopicResolver;
 import net.consensys.besu.plugins.stream.model.DefaultEvent;
 import net.consensys.besu.plugins.stream.model.DomainObjectType;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import java.nio.charset.StandardCharsets;
+
+import io.nats.client.Connection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(JUnitPlatform.class)
-@SuppressWarnings("rawtypes")
-public class KafkaPublisherTest {
-  @Mock private KafkaProducer<String, String> producer;
-  @Captor private ArgumentCaptor<ProducerRecord> record;
+public class NatsPublisherTest {
+
+  @Mock private Connection connectionMock;
 
   @Test
-  void build() {
-    final Publisher kafkaPublisher = KafkaPublisher.build(new KafkaPluginConfiguration());
-    assertNotNull(kafkaPublisher);
-  }
-
-  @Test
-  @SuppressWarnings({"MockitoInternalUsage", "unchecked"})
+  @SuppressWarnings("MockitoInternalUsage")
   void publish() throws Exception {
-    final Publisher kafkaPublisher = new KafkaPublisher(producer);
+    final Publisher natsPublisher = new NatsPublisher(connectionMock);
     final Event blockAddedEvent =
         DefaultEvent.create(
             "BlockAdded",
             "0xfe88c94d860f01a17f961bf4bdfb6e0c6cd10d3fda5cc861e805ca1240c58553",
             (mapper, payload) -> mapper.createObjectNode());
-    kafkaPublisher.publish(
+    natsPublisher.publish(
         DomainObjectType.BLOCK, new TopicResolver.Fixed(() -> "test-topic"), blockAddedEvent);
-    verify(producer).send(record.capture(), any());
-    assertThat(record.getValue().value()).isEqualTo(blockAddedEvent.string());
+    verify(connectionMock)
+        .publish(eq("test-topic"), eq(blockAddedEvent.string().getBytes(StandardCharsets.UTF_8)));
   }
 }
