55a576d9df745a64c93e0d0d8b380443318f147d
Karim TAAM <karim.t2am@gmail.com>
2021-03-25 15:08:40 +0100

    update repo name
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- scripts/cloudsmith-upload.sh
+++ scripts/cloudsmith-upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-LAUNCHER_VERSION=${1:?Must specify besu-plugins version}
-JAR=${2:?Must specify path to jar}
-POM=${3:?Must specify path to pom}
+PLUGINS_VERSION=${1:?Must specify plugins version}
+TAR_DIST=${2:?Must specify path to tar distribution}
+ZIP_DIST=${3:?Must specify path to zip distribution}
 
 ENV_DIR=./build/tmp/cloudsmith-env
 if [[ -d ${ENV_DIR} ]] ; then
@@ -15,4 +15,5 @@ fi
 
 python3 -m pip install --upgrade cloudsmith-cli
 
-cloudsmith push maven consensys/besu-plugins "${JAR}" --pom-file="${POM}" --version "1.0.1"
+cloudsmith push raw consensys/quorum-besu-plugins $TAR_DIST --name "besu-plugins-${PLUGINS_VERSION}.tar.gz" --version "${PLUGINS_VERSION}" --summary "Quorum Besu plugins ${PLUGINS_VERSION} binary distribution" --description "Binary distribution of Quorum Besu plugins ${PLUGINS_VERSION}." --content-type 'application/tar+gzip'
+cloudsmith push raw consensys/quorum-besu-plugins $ZIP_DIST --name "besu-plugins-${PLUGINS_VERSION}.zip" --version "${PLUGINS_VERSION}" --summary "Quorum Besu plugins ${PLUGINS_VERSION} binary distribution" --description "Binary distribution of Quorum Besu plugins ${PLUGINS_VERSION}." --content-type 'application/zip'
